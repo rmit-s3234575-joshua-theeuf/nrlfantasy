8d0bd61b08aed443b209ab84cd3ffa5dd06dbf86
deepozzzie <s3234575@student.rmit.edu.au>
2018-01-03 13:56:53 +1100

    basic login and logout done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Player Selector for 2018!"
       redirect_to @user
     else
